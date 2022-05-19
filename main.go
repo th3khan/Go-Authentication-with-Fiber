d10e1d5ebfe460da292beb1a6cd17fbbfbe4a0ce
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-05-19 17:14:47 -0400

    define paths

--- main.go
+++ main.go
@@ -5,6 +5,22 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
+	app.Post("/signup", func(c *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Get("/private", func(c *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Get("/publib", func(c *fiber.Ctx) error {
+		return nil
+	})
+
 	if err := app.Listen(":3000"); err != nil {
 		panic(err)
 	}
